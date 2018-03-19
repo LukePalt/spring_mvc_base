a9165d8a4e5f94dd44fadcfd5f78bd9c5dfff15f
Łukasz Palt <lukasz.palt@gmail.com>
2018-03-19 18:20:06 +0100

    Project adjustment to Spring Boot.

---
+++ src/main/java/com/pluralsight/FitnessTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTrackerApplication.class);
+    }
+}
